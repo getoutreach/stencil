0e3476c0a7fb366a514b22ad275fc848fb6e4f1d
Jared Allard <jared.allard@outreach.io>
2022-04-08 09:52:34 -0700

    fix(git): query remote for HEAD branch

--- internal/codegen/values_test.go
+++ internal/codegen/values_test.go
@@ -42,8 +42,8 @@ func TestValues(t *testing.T) {
 	assert.NilError(t, err, "expected worktree.Commit() not to fail")
 
 	err = wrk.Checkout(&gogit.CheckoutOptions{
-		Create: true,
-		Branch: plumbing.NewBranchReferenceName("main"),
+		//Create: true,
+		Branch: plumbing.NewBranchReferenceName("master"),
 	})
 	assert.NilError(t, err, "expected worktree.Checkout() not to fail")
 
@@ -56,7 +56,7 @@ func TestValues(t *testing.T) {
 	vals := NewValues(context.Background(), sm)
 	assert.DeepEqual(t, &Values{
 		Git: &git{
-			Ref:           plumbing.NewBranchReferenceName("main").String(),
+			Ref:           plumbing.NewBranchReferenceName("master").String(),
 			Commit:        cmt.String(),
 			Dirty:         false,
 			DefaultBranch: "main",
