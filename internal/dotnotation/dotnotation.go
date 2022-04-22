62a79d1a94ba4c954b0774b599719240a224472c
Jared Allard <jared.allard@outreach.io>
2022-04-21 17:41:41 -0700

    fix(dotnotation): support all map types

--- internal/dotnotation/dotnotation.go
+++ internal/dotnotation/dotnotation.go
@@ -14,33 +14,69 @@ import (
 )
 
 // Get looks up an entry in data by parsing the "key" into deeply nested keys, traversing it by "dots" in the key name.
-func Get(data map[interface{}]interface{}, key string) (interface{}, error) {
+func Get(data interface{}, key string) (interface{}, error) {
 	return get(data, key)
 }
 
-// get is a recursize function to get a field from a map[interface{}]interface{}
+// getFieldOnMap returns a field on a given map
+func getFieldOnMap(data interface{}, key string) (interface{}, error) {
+	dataVal := reflect.ValueOf(data)
+	dataTyp := dataVal.Type()
+	if dataTyp.Kind() != reflect.Map {
+		return nil, fmt.Errorf("data is not a map")
+	}
+
+	// iterate over the keys of the map
+	// converting them to the type of the key, when we find the key
+	// we return the value
+	iter := dataVal.MapRange()
+	for iter.Next() {
+		k := iter.Key()
+		v := iter.Value()
+
+		if k.Kind() == reflect.Interface {
+			// convert interface{} keys into their actual
+			// values
+			k = reflect.ValueOf(k.Interface())
+		}
+
+		// quick hack to convert all types to string :(
+		strK := fmt.Sprintf("%v", k.Interface())
+		fmt.Printf("%q ===	%q\n", strK, key)
+		if strK == key {
+			return v.Interface(), nil
+		}
+	}
+
+	return nil, fmt.Errorf("key %q not found", key)
+}
+
+// get is a recursive function to get a field from a map[interface{}]interface{}
 // this is done by splitting the key on "." and using the first part of the
 // split, if there is anymore parts of the key then get() is called with
 // the non processed part
-func get(data map[interface{}]interface{}, key string) (interface{}, error) {
+func get(data interface{}, key string) (interface{}, error) {
 	spl := strings.Split(key, ".")
 
-	partialKey := spl[0]
-	subDataInf, ok := data[partialKey]
-	if !ok {
-		return nil, fmt.Errorf("unknown key %q", partialKey)
+	v, err := getFieldOnMap(data, spl[0])
+	if err != nil {
+		return nil, err
 	}
 
-	// check if we have more "keys"
+	// check if we have more keys to iterate over
 	if len(spl) > 1 {
-		subData, ok := subDataInf.(map[interface{}]interface{})
-		if !ok {
-			return nil, fmt.Errorf("unsupported type %q on key %q", reflect.ValueOf(subDataInf).Type(), partialKey)
+		// pop the first key, and get the next value as the next key to
+		// process
+		nextKey := spl[1:][0]
+		nextDataTyp := reflect.TypeOf(v)
+		if nextDataTyp == nil || nextDataTyp.Kind() != reflect.Map {
+			return nil, fmt.Errorf("key %q is not a map, got %v on %q", nextKey, nextDataTyp, reflect.TypeOf(data))
 		}
 
-		return get(subData, strings.Join(spl[1:], "."))
+		// pop the first key, and call get() again
+		return get(v, strings.Join(spl[1:], "."))
 	}
 
 	// otherwise return the data
-	return subDataInf, nil
+	return v, nil
 }
