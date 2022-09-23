e9ea795e48ece43a1e5c40a323726f9e1b482fa3
Jared Allard <jaredallard@outlook.com>
2022-09-23 11:15:28 -0700

    unit test for new functionality

--- cmd/stencil/describe.go
+++ cmd/stencil/describe.go
@@ -22,35 +22,52 @@ func NewDescribeCmd() *cli.Command {
 		Name:        "describe",
 		Description: "Print information about a known file rendered by a template",
 		Action: func(c *cli.Context) error {
-			l, err := stencil.LoadLockfile("")
-			if err != nil {
-				return errors.Wrap(err, "failed to load lockfile")
+			if c.NArg() != 1 {
+				return errors.New("expected exactly one argument, path to file")
 			}
 
-			// make absolute so we can handle .. and other weird path things
-			// defaults to nothing if already absolute
-			filePath, err := filepath.Abs(c.Args().First())
-			if err != nil {
-				return errors.Wrap(err, "failed to get absolute path")
-			}
-
-			// convert absolute -> relative
-			cwd, err := os.Getwd()
-			if err != nil {
-				return errors.Wrap(err, "failed to get current working directory")
-			}
-			filePath = "." + strings.TrimPrefix(filePath, cwd)
-
-			// ensure that we don't have any weird path elements
-			relativeFilePath := filepath.Clean(filePath)
-			for _, f := range l.Files {
-				if f.Name == relativeFilePath {
-					fmt.Printf("%s was created by module https://%s (template: %s)\n", f.Name, f.Module, f.Template)
-					return nil
-				}
-			}
-
-			return fmt.Errorf("file %q isn't created by stencil", filePath)
+			return describeFile(c.Args().First())
 		},
 	}
+}
+
+// cleanPath ensures that a path is always relative to the current working directory
+// with no .., . or other path elements.
+func cleanPath(path string) (string, error) {
+	// make absolute so we can handle .. and other weird path things
+	// defaults to nothing if already absolute
+	path, err := filepath.Abs(path)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get absolute path")
+	}
+
+	// convert absolute -> relative
+	cwd, err := os.Getwd()
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get current working directory")
+	}
+	path = "." + strings.TrimPrefix(path, cwd)
+	return filepath.Clean(path), nil
+}
+
+// describeFile prints information about a file rendered by a template
+func describeFile(filePath string) error {
+	l, err := stencil.LoadLockfile("")
+	if err != nil {
+		return errors.Wrap(err, "failed to load lockfile")
+	}
+
+	relativeFilePath, err := cleanPath(filePath)
+	if err != nil {
+		return errors.Wrap(err, "failed to clean path for searching lockfile")
+	}
+
+	for _, f := range l.Files {
+		if f.Name == relativeFilePath {
+			fmt.Printf("%s was created by module https://%s (template: %s)\n", f.Name, f.Module, f.Template)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("file %q isn't created by stencil", filePath)
 }
