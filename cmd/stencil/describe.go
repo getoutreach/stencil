36c15d1304fac71997be932a94e0f1c9bedb8d8c
Jared Allard <jaredallard@outlook.com>
2022-09-23 11:03:21 -0700

    fix(describe): support abs and other path elements

--- cmd/stencil/describe.go
+++ cmd/stencil/describe.go
@@ -6,6 +6,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/getoutreach/stencil/pkg/stencil"
 	"github.com/pkg/errors"
@@ -24,15 +27,30 @@ func NewDescribeCmd() *cli.Command {
 				return errors.Wrap(err, "failed to load lockfile")
 			}
 
-			fileName := c.Args().First()
+			// make absolute so we can handle .. and other weird path things
+			// defaults to nothing if already absolute
+			filePath, err := filepath.Abs(c.Args().First())
+			if err != nil {
+				return errors.Wrap(err, "failed to get absolute path")
+			}
+
+			// convert absolute -> relative
+			cwd, err := os.Getwd()
+			if err != nil {
+				return errors.Wrap(err, "failed to get current working directory")
+			}
+			filePath = "." + strings.TrimPrefix(filePath, cwd)
+
+			// ensure that we don't have any weird path elements
+			relativeFilePath := filepath.Clean(filePath)
 			for _, f := range l.Files {
-				if f.Name == fileName {
+				if f.Name == relativeFilePath {
 					fmt.Printf("%s was created by module https://%s (template: %s)\n", f.Name, f.Module, f.Template)
 					return nil
 				}
 			}
 
-			return fmt.Errorf("file %q isn't created by stencil", fileName)
+			return fmt.Errorf("file %q isn't created by stencil", filePath)
 		},
 	}
 }
