0c6daa1d53ea0d715a94bafdca8f00e56d25d0db
Jared Allard <jaredallard@outlook.com>
2022-10-17 11:27:00 -0700

    add extensions test

---
+++ pkg/extensions/extensions_test.go
@@ -0,0 +1,32 @@
+package extensions_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/getoutreach/stencil/pkg/extensions"
+	"github.com/sirupsen/logrus"
+	"gotest.tools/v3/assert"
+)
+
+func TestCanImportNativeExtension(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ext := extensions.NewHost(logrus.New())
+	defer ext.Close()
+
+	err := ext.RegisterExtension(ctx, "https://github.com/getoutreach/stencil-golang", "github.com/getoutreach/stencil-golang", "v1.3.0")
+	assert.NilError(t, err, "failed to register extension")
+
+	caller, err := ext.GetExtensionCaller(ctx)
+	assert.NilError(t, err, "failed to get extension caller")
+
+	resp, err := caller.Call("github.com/getoutreach/stencil-golang.ParseGoMod", "go.mod", "module test\n\ngo 1.19")
+	assert.NilError(t, err, "failed to call extension")
+
+	moduleMap := resp.(map[string]interface{})["Module"].(map[string]interface{})
+	spew.Dump(moduleMap)
+	assert.Equal(t, moduleMap["Syntax"].(map[string]interface{})["Token"].([]interface{})[1], "test", "failed to parse go.mod")
+}
