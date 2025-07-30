71f8f18145a4808d0267c0e27c1bca57be4ea423
Mark Lee <mark.lee@outreach.io>
2025-07-29 22:56:41 -0700

    ci: update before install

--- scripts/test-pre-setup.sh
+++ scripts/test-pre-setup.sh
@@ -3,4 +3,5 @@
 # Custom script to set up the environment before running tests.
 
 # This is needed to ensure that the French locale is available for tests.
+sudo apt-get update
 sudo apt-get install --yes language-pack-fr
