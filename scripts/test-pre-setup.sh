0b73f0bfa782c9146d48a299b86d9592dd1c40ae
Mark Lee <mark.lee@outreach.io>
2025-07-29 19:13:04 -0700

    ci: add test pre-setup script

---
+++ scripts/test-pre-setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# Custom script to set up the environment before running tests.
+
+# This is needed to ensure that the French locale is available for tests.
+sudo apt-get install --yes language-pack-fr
